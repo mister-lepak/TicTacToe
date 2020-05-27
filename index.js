2764998f02807ca60ac6c3e12afc11a4ba69e19e
Try Wijono <3wijono@gmail.com>
2020-05-27 21:56:47 +0800

    fixed css to grid layout

--- index.js
+++ index.js
@@ -6,60 +6,66 @@ let winA = 0;
 let winB = 0;
 let scoreCheck = 0;
 
+// Render Player Scores
 const playerScores = () => {
-  const scoreBox = document.createElement('div');
-  scoreBox.classList.add('score');
-  scoreBox.innerHTML = `
-    <h2>
-      <div id="scoreA">
-      PlayerA
-      <br>${winA}</br>
-      </div>
-      <div id="scoreB">
-      PlayerB
-      <br>${winB}</br>
-      </div>
-    </h2>
+  const scoreBoxA = document.createElement('div');
+  scoreBoxA.innerHTML = `
+    <h2>Player A</h2>
+    <h2>${winA}</h2>
     `;
-  document.querySelector('#main').appendChild(scoreBox);
+  document.querySelector('#scoreA').append(scoreBoxA);
+
+  const scoreBoxB = document.createElement('div');
+  scoreBoxB.innerHTML = `
+    <h2>Player B</h2>
+    <h2>${winB}</h2>
+    `;
+  document.querySelector('#scoreB').append(scoreBoxB);
 };
 
+// Cleanup Player Scores
 const cleanPlayerScores = () => {
-  const score = document.querySelector('.score');
-  score.remove();
+  const scoreA = document.querySelector('#scoreA').querySelector('div');
+  scoreA.remove();
+  const scoreB = document.querySelector('#scoreB').querySelector('div');
+  scoreB.remove();
 };
-
-playerScores();
-
 
 // Define and Create grid boxes
-const gridBox = document.querySelector('#grid');
-for ( i=0 ; i<gridSize ; i++ ){
-  const vertical = document.createElement('div');
-  vertical.classList.add('verticalBox');
-  for ( j=0 ; j<gridSize ; j++ ){
-    const horizontal = document.createElement('div');
-    horizontal.classList.add('horizontalBox');
-    vertical.append(horizontal);
+const renderGrid = () => {
+  const gridBox = document.querySelector('#grid');
+  for ( i=0 ; i<gridSize ; i++ ){
+    const vertical = document.createElement('div');
+    vertical.classList.add('verticalBox');
+    vertical.style.width = `${window.innerWidth*(1/(gridSize**2))}px`;
+    vertical.style.height = `${window.innerWidth*(1/(gridSize**2))}px`;
+    for ( j=0 ; j<gridSize ; j++ ){
+      const horizontal = document.createElement('div');
+      horizontal.classList.add('horizontalBox');
+      horizontal.style.width = `${window.innerWidth*(1/(gridSize**2))}px`;
+      horizontal.style.height = `${window.innerWidth*(1/(gridSize**2))}px`;
+      vertical.append(horizontal);
 
-    horizontal.addEventListener('click', ()=> {
-      if(horizontal.classList[1]){}
-      else {
-        if (user == 1) {
-          horizontal.classList.add('activeA');
-          user--;
-          pointA++;
-        } else {
-          horizontal.classList.add('activeB');
-          user++;
-          pointB++;
+      horizontal.addEventListener('click', ()=> {
+        if(horizontal.classList[1]){}
+        else {
+          if (user == 1) {
+            horizontal.classList.add('activeA');
+            user--;
+            pointA++;
+          } else {
+            horizontal.classList.add('activeB');
+            user++;
+            pointB++;
+          }
         }
-      }
-    });
+      });
+    };
+    gridBox.append(vertical);
   };
-  gridBox.append(vertical);
 };
 
+// Function definition when the one of the player has won the game
 const playerUltiWin = (player) => {
   document.querySelector('#notice').innerHTML = `
     <div class="notice">
@@ -85,13 +91,14 @@ const playerUltiWin = (player) => {
       winB = 0;
       cleanPlayerScores();
       playerScores();
-      document.querySelector('#grid').classList.remove('end');
+      document.querySelector('.parent').classList.remove('end');
     });
   });
 
-  document.querySelector('#grid').classList.add('end');
+  document.querySelector('.parent').classList.add('end');
 }
 
+// Function definition when the one of the player wins
 const playerWins = (player) => {
   document.querySelector('#notice').innerHTML = `
     <div class="notice">
@@ -113,17 +120,18 @@ const playerWins = (player) => {
       document.querySelector('#notice').innerHTML = '';
       pointA = 0;
       pointB = 0;
-      document.querySelector('#grid').classList.remove('end');
+      document.querySelector('.parent').classList.remove('end');
     });
   });
   if (`win${player}` === "winA") winA++;
   else if(`win${player}` === "winB") winB++;
   cleanPlayerScores();
   playerScores();
-  document.querySelector('#grid').classList.add('end');
+  document.querySelector('.parent').classList.add('end');
   scoreCheck = 1;
 }
 
+// Function definition when the game is draw
 const playerDraw = () => {
   document.querySelector('#notice').innerHTML = `
   <div class="notice">
@@ -145,10 +153,10 @@ const playerDraw = () => {
       document.querySelector('#notice').innerHTML = '';
       pointA = 0;
       pointB = 0;
-      document.querySelector('#grid').classList.remove('end');
+      document.querySelector('.parent').classList.remove('end');
     });
   });
-  document.querySelector('#grid').classList.add('end');
+  document.querySelector('.parent').classList.add('end');
 };
 
 // Perform validation on every clicks
@@ -207,3 +215,7 @@ document.body.addEventListener('click', () => {
   else if(winB === 3) playerUltiWin('B');
   scoreCheck=0;
 });
+
+// Initialization
+playerScores();
+renderGrid();
